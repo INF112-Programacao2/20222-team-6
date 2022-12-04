16f7f75e6b5bb12c9f2570670a3ba484fc8855eb
Calambau <marco.a.cunha@ufv.br>
2022-12-04 03:03:53 -0300

    Criando a classe Acao

---
+++ Acao.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "Acao.h"
+
+
+Acao::Acao(){}
+
+Acao::~Acao(){}
+
+void Acao::defineAtack(/*Herois* heroi, Herois* inimigo*/){
+    /*
+    int actualHP;
+    actualHP = inimigo->getHP() - heroi->getAtack();
+    inimigo->setHP(actualHP);
+    
+    
+    
+    
+    */
+}
+
+void Acao::defineDefense(/*Herois* heroi*/){
+    /*
+    int damageResistence = 30;
+    damageResistence += heroi->getDefense();
+    heroi->setDefense(damageResistence);
+
+    
+    
+    
+    */
+
+
+}
+
+void Acao::defineHeal(/*Herois* heroi*/){
+    /*
+    int Heal = 20;
+    Heal += heroi->getHP();
+    heroi->setHP(Heal);
+    
+    
+    */
+
+
+
+
+}
---
+++ Acao.h
@@ -0,0 +1,22 @@
+#ifndef ACAO_H
+#define ACAO_H
+
+class Acao
+{
+
+
+    public:
+    Acao();
+    ~Acao();
+    void defineAtack(/*Herois* heroi, Herois* inimigo*/);
+    void defineDefense(/*Herois* heroi*/);
+    void defineHeal(/*Herois* heroi*/);
+    
+
+
+
+
+
+};
+
+#endif 
