47caa437de7a47f24f118949abe76545cfafe97a
Pedrohbcarvalho <pedrohbc1504@gmail.com>
2022-12-10 20:00:50 -0300

    Implementacao do tratamento de excecoes em Personagens.cpp

--- Personagens.cpp
+++ Personagens.cpp
@@ -21,72 +21,200 @@ void Personagens::movement() {
         if (contador > 20) contador = 0;
             if (contador >= 0 && contador < 5 && teclas[0] > teclas[2]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-back.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-back.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador >= 0 && contador < 5 && teclas[0] < teclas[2]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-front.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-back.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador > 5 && contador < 10 && teclas[0] > teclas[2]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-back1.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-back.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador > 10 && contador < 15 && teclas[0] > teclas[2]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-back.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-back.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador > 15 && contador < 20 && teclas[0] > teclas[2]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-back2.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-back.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador > 5 && contador < 10 && teclas[0] < teclas[2]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-front1.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-back.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador > 10 && contador < 15 && teclas[0] < teclas[2]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-front.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-back.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador > 15 && contador < 20 && teclas[0] < teclas[2]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-front2.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-back.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
         }
-    if (teclas[1] == 1 || teclas[3] == 1) {      /* Movimento do W e do S */
+    if (teclas[1] == 1 || teclas[3] == 1) {      /* Movimento do A e do D */
         personagemS.move(( teclas[3] - teclas[1]) * 2,0);
         contador += 1.f;
         if (contador > 20) contador = 0;
             if (contador >= 0 && contador < 5 && teclas[1] > teclas[3]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-side-left.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-side-left.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador >= 0 && contador < 5 && teclas[1] < teclas[3]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-side-right.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-side-right.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador > 5 && contador < 10 && teclas[1] > teclas[3]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-side-left1.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-side-left1.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador > 10 && contador < 15 && teclas[1] > teclas[3]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-side-left.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-side-left.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador > 15 && contador < 20 && teclas[1] > teclas[3]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-side-left2.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-side-left2.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador > 5 && contador < 10 && teclas[1] < teclas[3]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-side-right1.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-side-right1.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador > 10 && contador < 15 && teclas[1] < teclas[3]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-side-right.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-side-right.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
             if (contador > 15 && contador < 20 && teclas[1] < teclas[3]) {
                 personagemT.loadFromFile("sprites/anao/dwarf-side-right2.png");
+                if (personagemT.loadFromFile("sprites/anao/dwarf-side-right2.png") == false){
+                    throw std::invalid_argument("Houve um erro ao carregar a sprite");
+                }
+                try{
                 personagemS.setTexture(personagemT);
+                } 
+                catch (std::invalid_argument &e){
+                std::cout << e.what();
+                }
             }
         }
 }
